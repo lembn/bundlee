1e5391a16d342ccccc22c65fa1e0c188d1551c4c
lembn <lembn@outlook.com>
2021-06-29 23:16:57 +0100

    Added and implemented inquirer

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # js-bundler
 
-Bundle source code and npm modules into a distribution folder
+A CLI to bundle source code and node modules into a distribution folder
 
 # TODO
 
-- Convert from args to inquire and add a -Y mode for defualts
-- Add automatic local dependency update
 - Add progress bar
-- Add colours
+- Add colours (winston?)
+- Add automatic local dependency update
 - publish
 
 # Notes
+
+`js-bunlder` was made to make projects with local dependencies easier to deploy. Developing code with local imports usually requires using relative paths to load modules. A devloper could get around this by publishing their local packages to the npm registry. This means that whenever the support packages can be installed into the main package, and added to the `package.json` so that when the main package is intalled the support packages come along with it. However this can be alot of work for a package that is still in development, private, or changing often. This devloper might avoid publishing the package and use `npm install --save [path]` to bring the local support package into the main packages, `node_modules` folder, but this is also a hassle - especially if the support package in in development, since every time the support package is changed the main package needs to update itself. While this can be automated with file watchers and some scripting, it doesn't change the fact that the support package is still local, so will not be accessible to a remote deployment. `js-bundler` aims to solve all these problems, by automatically collecting and updating these local packages, and bundling them with the main source code and other node packages into a single folder, which can be deployed without needing to publish the local packages or copy them into the main package.
 
 `js-bunlder` requires all the package's source code to be in one folder. Likewise, all installed node modules should be contained within a single folder. The contents of these folders are copied into the `dist` folder with their internal structure preserved. Any local libraries used in the module should be packaged into a node package (with `npm init`) and installed as a local dependency so they will be contained within the main package's node modules.
--- src/bundler.js
+++ src/bundler.js
@@ -1,41 +1,85 @@
 const fs = require("fs-extra");
+const inquirer = require("inquirer");
+const isValid = require("is-valid-path");
+const yargs = require("yargs");
 
-const argv = require("yargs")
-  .alias("o", "output")
-  .alias("s", "src")
-  .alias("m", "modules")
-  .alias("h", "help")
-  .help("h")
-  .usage("Usage: $0 [options]")
-  .describe("o", "Specify build path output location. Default: ./dist")
-  .describe("s", "Specify source code folder. Default: ./src")
-  .describe("m", "Specify node_modules folder. Default: ./node_modules").argv;
+yargs.scriptName("bundler");
+yargs.option("y", {
+  alias: "yes",
+  describe: "Use default settings.",
+  type: "boolean",
+  normalize: true,
+});
+yargs.help();
+yargs.alias("h", "help");
+yargs.alias("v", "version");
+const { argv } = yargs;
 
-var { output = "./dist", src = "./src", modules = "./node_modules" } = argv;
+const defaults = {
+  output: "./dist",
+  src: "./src",
+  modules: "./node_modules",
+};
 
-output = output.replace(/\\/g, "/");
-src = src.replace(/\\/g, "/");
-modules = modules.replace(/\\/g, "/");
+const convertBackslashes = (input) => input.replace(/\\/g, "/");
 
-const srcLoc = `${output}/${src.split("/").pop()}`;
-const modulesLoc = `${output}/${modules.split("/").pop()}`;
+const validate = (input) => {
+  valid = isValid(input);
+  if (valid) return true;
+  else return "Please enter a valid folder path";
+};
 
-if (fs.existsSync(output)) {
-  console.log("Output folder found.");
-  if (!fs.existsSync(srcLoc)) fs.mkdirSync(srcLoc);
-  if (!fs.existsSync(modulesLoc)) fs.mkdirSync(modulesLoc);
-} else {
-  console.log(`Output location: '${output}' not found.`);
-  console.log(`Creating:\n${output}\n${srcLoc}\n${modulesLoc}`);
-  fs.mkdirSync(output);
-  fs.mkdirSync(srcLoc);
-  fs.mkdirSync(modulesLoc);
-  console.log("Created output location");
+async function setup() {
+  return await inquirer.prompt([
+    {
+      name: "output",
+      message: "Where should the bundle be generated?: ",
+      default: defaults.output,
+      validate: validate,
+      filter: convertBackslashes,
+    },
+    {
+      name: "src",
+      message: "Where should is the source code located?: ",
+      default: defaults.src,
+      validate: validate,
+      filter: convertBackslashes,
+    },
+    {
+      name: "modules",
+      message: "Where should is the 'node_modules' folder?: ",
+      default: defaults.modules,
+      validate: validate,
+      filter: convertBackslashes,
+    },
+  ]);
 }
 
-console.log("Bundling");
+async function main(useDefaults) {
+  const { output, src, modules } = useDefaults ? defaults : await setup();
 
-fs.copySync(src, srcLoc, { overwrite: true });
-fs.copySync(modules, modulesLoc, { overwrite: true });
+  const srcLoc = `${output}/${src.split("/").pop()}`;
+  const modulesLoc = `${output}/${modules.split("/").pop()}`;
 
-console.log("Bundle Complete.");
+  if (fs.existsSync(output)) {
+    console.log("Output folder found.");
+    if (!fs.existsSync(srcLoc)) fs.mkdirSync(srcLoc);
+    if (!fs.existsSync(modulesLoc)) fs.mkdirSync(modulesLoc);
+  } else {
+    console.log(`Output location: '${output}' not found.`);
+    console.log(`Creating:\n${output}\n${srcLoc}\n${modulesLoc}`);
+    fs.mkdirSync(output);
+    fs.mkdirSync(srcLoc);
+    fs.mkdirSync(modulesLoc);
+    console.log("Created output location");
+  }
+
+  console.log("Bundling");
+
+  fs.copySync(src, srcLoc, { overwrite: true });
+  fs.copySync(modules, modulesLoc, { overwrite: true });
+
+  console.log("Bundle Complete.");
+}
+
+main(argv.y);
